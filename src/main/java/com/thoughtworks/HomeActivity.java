2993ee910587ecb9e2b2c501fa7d8ce1efb198af
geechrist <geechrist@gmail.com>
2013-06-06 09:10:20 +0800

    init commit

---
+++ src/main/java/com/thoughtworks/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.thoughtworks;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class HomeActivity extends SherlockFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+}
+
